88eb1afdeef47d96f6faa66e23ede509714cb714
daneelsjo <daneelsjo88@gmail.com>
2025-11-19 14:37:01 +0100

    fix: remove test comment for DEV deployment

--- public/script/javascript/main.js
+++ public/script/javascript/main.js
@@ -8,7 +8,6 @@ import {
   query, where
 } from "./firebase-config.js";
 
-//kleine testwijziging voor DEVELOP branch - test deployment - test 3
 /* ────────────────────────────────────────────────────────────────────────────
    Firebase init
    ──────────────────────────────────────────────────────────────────────────── */
